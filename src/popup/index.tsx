737da7a3b93caf5a6a0d19f79aa240f2ea1e1032
monkjiro <thenullmastermind@gmail.com>
2024-07-04 19:09:19 +0700

    refactor: Remove old Popup.css and update Popup.tsx functionality

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Popup } from "./Popup";
 import "./index.css";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
-    <Popup />
+    <MantineProvider theme={theme} defaultColorScheme={"dark"}>
+      <Popup />
+    </MantineProvider>
   </React.StrictMode>,
 );
